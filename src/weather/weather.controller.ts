c831ed48d6c131eca2b757ce5a31b2107e441370
JaeMin Kim <jeffthehero038@gmail.com>
2025-01-29 21:49:59 +0900

    주석 추가/삭제

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -16,20 +16,3 @@ export class WeatherController {
     return this.weatherService.getWeatherByLocation(region);
   }
 }
-// import { Controller, Get, Query } from '@nestjs/common';
-// import { WeatherService } from './weather.service';
-
-// @Controller('weather')
-// export class WeatherController {
-//   constructor(private readonly weatherService: WeatherService) {}
-
-//   @Get()
-//   async getWeather(
-//     // @Query('date') date: string,
-//     // @Query('time') time: string,
-//     @Query('nx') nx: number,
-//     @Query('ny') ny: number,
-//   ) {
-//     return this.weatherService.getWeather(nx, ny);
-//   }
-// }
