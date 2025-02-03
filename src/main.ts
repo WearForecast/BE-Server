305b42ae79cd391e6636ae96541329a671579e34
JaeMin Kim <jeffthehero038@gmail.com>
2025-02-03 11:41:25 +0900

    CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors(); // Enable CORS
+
   const config = new DocumentBuilder()
     .setTitle('WearForecast Server')
     .setDescription('WearForecast API description')
